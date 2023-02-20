40eb2bb2fe3ba51ec2bfa7caa38ddd8e1861d118
Elias Kirkwood <yannis3736@gmail.com>
2023-02-20 21:47:43 +0100

    Remove useless JS file

--- registering/index.php
+++ registering/index.php
@@ -13,8 +13,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;700;800;900&display=swap" rel="stylesheet">
 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-
-    <script src="./form.js" defer></script>
+    
     <title>Ronde de l'Espoir</title>
 </head>
 <body>
