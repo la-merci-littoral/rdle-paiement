ea188928f04e6cf6bebb3d31c4cfc37abf53e365
Elias Kirkwood <yannis3736@gmail.com>
2023-02-20 22:19:59 +0100

    Correct path to ICO images

--- choosing/index.php
+++ choosing/index.php
@@ -5,12 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <link rel="shortcut icon" href="../../../main/img/LRDE-logo.png" type="image/x-icon">
+    <link rel="shortcut icon" href="../../main/img/LRDE-logo.png" type="image/x-icon">
 
     <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;700;800;900&display=swap" rel="stylesheet">
+
 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <title>Ronde de l'Espoir</title>
