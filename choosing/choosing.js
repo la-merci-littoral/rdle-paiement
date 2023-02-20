1547a3bc54af37bbc3594e8e9848c8438e4e87e5
Elias Kirkwood <yannis3736@gmail.com>
2023-02-20 22:54:31 +0100

    Add cool effect to highlight words

--- choosing/choosing.js
+++ choosing/choosing.js
@@ -1,1 +1,23 @@
-console.log("Hello world!");
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+document.querySelectorAll('.highlight').forEach(element => {
+    element.onmouseover = event => {
+        let iterations = 0;
+
+        const interval = setInterval(() => {
+            event.target.innerText = event.target.innerText.split("")
+                .map((letter, index) => {
+                    if (index < iterations) {
+                        return event.target.dataset.value[index]
+                    }
+
+                    return letters[Math.floor(Math.random() * 26)]
+                })
+                .join("");
+
+                if (iterations >= 15) clearInterval(interval);
+
+                iterations += 1;
+        }, 30)
+    }
+})
