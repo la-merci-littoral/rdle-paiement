344cc76e5faeb8176fbba80f4f122bd56fb137f6
Elias Kirkwood <yannis3736@gmail.com>
2023-02-20 22:54:31 +0100

    Add cool effect to highlight words

--- choosing/index.php
+++ choosing/index.php
@@ -11,9 +11,11 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;700;800;900&display=swap" rel="stylesheet">
-
-
+    <link href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
+    
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+
+    <script src="choosing.js" defer></script>
     <title>Ronde de l'Espoir</title>
 </head>
 <body>
@@ -26,15 +28,15 @@
     <main>
         <ul>
             <li id="individual">
-                <p> Je participe <span class="highlight">maintenant</span>!</p>
+                <p> Je participe <span class="highlight" data-value="maintenant">maintenant</span>!</p>
                 <div><a class="material-symbols-outlined" href="../registering/">arrow_forward_ios </a></div>
             </li>
             <li id="individual anonymous">
-                <p> Je souhaite participer <span class="highlight">anonymement</span>.</p>
+                <p> Je souhaite participer <span class="highlight" data-value="anonymement">anonymement</span>.</p>
                 <div><a class="material-symbols-outlined" href="#">arrow_forward_ios </a></div>
             </li>
             <li id="company">
-                <p> Participer en tant qu'<span class="highlight">entreprise</span>.</p>
+                <p> Participer en tant qu'<span class="highlight" data-value="entreprise">entreprise</span>.</p>
                 <div><a class="material-symbols-outlined" href="#">arrow_forward_ios </a></div>
             </li>
         </ul>
