ec20a787ef0857c6db7ef5ef35607779aa6d9d62
Elias Kirkwood <yannis3736@gmail.com>
2023-02-20 23:10:09 +0100

    Add color to highlight (to confirm with design)

--- choosing/choosing.js
+++ choosing/choosing.js
@@ -1,4 +1,36 @@
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const colors = ["#E072A4",
+    "#3D3B8E",
+    "#4C1E4F",
+    "#4A8FE7",
+    "#4C2E05",
+    "#EE4266",
+    "#3CBBB1",
+    "#F07167",
+    "#90C3C8",
+    "#7CC6FE",
+    "#23C9FF",
+    "#9448BC",
+    "#512D38",
+    "#4A5159",
+    "#F4B860",
+    "#C83E4D",
+    "#720E07",
+    "#45050C",
+    "#58BC82",
+    "#801A86",
+    "#5C80BC",
+    "#7EBC89",
+    "#49DCB1",
+    "#456990",
+    "#A7A2A9",
+    "#222823",
+    "#9FC490",
+    "#011936",
+    "#A1674A",
+    "#D3B88C",
+    "#523249"
+]
 
 document.querySelectorAll('.highlight').forEach(element => {
     element.onmouseover = event => {
@@ -15,7 +47,12 @@ document.querySelectorAll('.highlight').forEach(element => {
                 })
                 .join("");
 
-                if (iterations >= 15) clearInterval(interval);
+                const colorId = Math.floor(Math.random() * 30);
+                console.log(colorId);
+                const color = colors[colorId];
+                event.target.style.color = color;
+
+                if (iterations >= event.target.dataset.value.length) clearInterval(interval);
 
                 iterations += 1;
         }, 30)
