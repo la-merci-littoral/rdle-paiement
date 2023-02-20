36648c1aec048dcf5468d6a0146f478bfeadd9d7
Elias Kirkwood <yannis3736@gmail.com>
2023-02-20 20:43:23 +0100

    Move nav component

---
+++ modules/nav.php
@@ -0,0 +1,13 @@
+<link rel="stylesheet" href="nav-style.css">
+<header>
+    <div class="header-wrapper">
+        
+        <span class="title">La Ronde de l'Espoir</span>
+        <span class="arrow">
+            <a href="https://ronde-de-l-espoir.fr">
+                <span class="material-symbols-outlined">arrow_back</span>
+            </a>
+        </span>
+
+    </div>
+</header>
