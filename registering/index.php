e5fd7ea6030f53cba3e2417c24ceeffbae54850d
Elias Kirkwood <yannis3736@gmail.com>
2023-02-20 17:28:05 +0100

    Added basis for form

---
+++ registering/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="../../../main/img/LRDE-logo.png" type="image/x-icon">
+
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;700;800;900&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+
+    <script src="./form.js" defer></script>
+    <title>Ronde de l'Espoir</title>
+</head>
+<body>
+
+    <header>
+        <div class="header-wrapper">
+
+            <span class="back">
+                <a href="https://ronde-de-l-espoir.fr">
+                    <span class="material-symbols-outlined">arrow_back</span>
+                </a>
+            </span>
+            <span class="title">La Ronde de l'Espoir</span>
+
+        </div>
+    </header>
+    
+    <main></main>
+    
+</body>
+</html>
