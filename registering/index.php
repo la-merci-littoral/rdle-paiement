b22e3a55c4e7a1003ba6b055de9dc5bd93afe9ca
Elias Kirkwood <yannis3736@gmail.com>
2023-02-21 22:40:29 +0100

    Removed STUPID error of local tests :(

--- registering/index.php
+++ registering/index.php
@@ -1,11 +1,6 @@
 <?php
     session_start();
 
-    require('../config/tempo_db.php');
-    // if (isset($_SESSION['lname'])) {
-    //     echo $_SESSION['lname'];
-    // }
-
     $lname = isset($_SESSION['lname']) ? $_SESSION['lname'] : "";
     $fname = isset($_SESSION['fname']) ? $_SESSION['fname'] : "";
     $postal = isset($_SESSION['postal']) ? $_SESSION['postal'] : "";
