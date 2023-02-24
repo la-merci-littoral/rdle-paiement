83d384cb97613333c8bc1102583cc2f3ad2832e9
Silloky <ek.mindstorms@outlook.com>
2023-02-24 18:31:35 +0100

    Refactor : deleted old code

--- anonymous/anonymous.js
+++
@@ -1,8 +0,0 @@
-function toggleSubmit(val){
-    var sbmtBtn = document.getElementsByName("submit")[0]
-    if (val.checked){
-        sbmtBtn.disabled = false
-    } else {
-        sbmtBtn.disabled = true
-    }
-}
