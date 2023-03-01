fed81a088c4893c41cefa8535b8a94f17fb73095
Silloky <ek.mindstorms@outlook.com>
2023-03-01 08:06:54 +0100

    Progress bar foundations

---
+++ modules/progress-bar.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Progress bar test</title>
+    <link rel="stylesheet" href="./progress-bar.css">
+</head>
+<body>
+    <div class="container">
+        <ul class="progressbar">
+            <li class="done">Step 1</li>
+            <li class="active">Step 2</li>
+            <li>Step 3</li>
+        </ul>
+    </div>
+</body>
+</html>
