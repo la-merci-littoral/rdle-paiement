c6d7c5d7798e0913bb72f2445006afcecfafed95
Elias Kirkwood <yannis3736@gmail.com>
2023-03-05 13:51:07 +0100

    Return to 1st test

--- modules/progress.php
+++ modules/progress.php
@@ -1,8 +1,7 @@
 <link rel="stylesheet" href="../modules/progress-style.css">
 <div class="progress-wrapper">
-    <!-- <div class="progress-bar">
+    <div class="progress-bar">
         <ul>
-            <li>Choix</li>
             <li>Montant</li>
             <li>Paiement</li>
             <li>Terminé !</li>
@@ -10,20 +9,19 @@
     </div>
     <div class="progress-bar">
         <ul>
-            <li>Choix</li>
             <li>Informations</li>
             <li>Montant</li>
             <li>Paiement</li>
             <li>Terminé !</li>
         </ul>
-    </div> -->
+    </div>
 
-    <div class="progress">
+    <!-- <div class="progress">
         <ul>
             <li class="step">1</li>
             <li class="step">2</li>
             <li class="step">3</li>
             <li class="step">4</li>
         </ul>
-    </div>
+    </div> -->
 </div>
