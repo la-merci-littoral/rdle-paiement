851f6c439cf7e4fd3c5b7f369827a77a3aad8deb
Elias Kirkwood <yannis3736@gmail.com>
2023-03-05 14:47:20 +0100

    Upgrade logical component to better-looking icon

--- modules/progress.php
+++ modules/progress.php
@@ -1,24 +1,27 @@
 <link rel="stylesheet" href="../modules/progress-style.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 <div class="progress-wrapper">
     <div class="progress-bar">
         <ul>
             <li class="step done-text"><span class=" done numeric-indicator">1</span>Montant</li>
-            <li class="logic-component"> > </li>
+            <li class="logic-component"><span class="material-symbols-outlined">navigate_next</span></li>
+
             <li class="step current-text"><span class="current numeric-indicator">2</span>Paiement</li>
-            <li class="logic-component"> > </li>
+            <li class="logic-component"><span class="material-symbols-outlined">navigate_next</span></li>
+
             <li class="step"><span class="numeric-indicator">3</span>Terminé !</li>
         </ul>
     </div>
     <div class="progress-bar">
         <ul>
             <li class="step done-text"><span class="done numeric-indicator">1</span> Informations</li>
-            <li class="logic-component"> > </li>
+            <li class="logic-component"><span class="material-symbols-outlined">navigate_next</span></li>
 
             <li class="step done-text"><span class="done numeric-indicator">2</span>Montant</li>
-            <li class="logic-component"> > </li>
+            <li class="logic-component"><span class="material-symbols-outlined">navigate_next</span></li>
 
             <li class="step current-text"><span class="current numeric-indicator">3</span>Paiement</li>
-            <li class="logic-component"> > </li>
+            <li class="logic-component"><span class="material-symbols-outlined">navigate_next</span></li>
 
             <li class="step"><span class="numeric-indicator">4</span>Terminé !</li>
         </ul>
