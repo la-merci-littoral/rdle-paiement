8d54e87ed6128abe8a504fe3ad817e16e339e54f
Elias Kirkwood <yannis3736@gmail.com>
2023-03-05 14:17:40 +0100

    Add classes to steps to test style

--- modules/progress.php
+++ modules/progress.php
@@ -2,9 +2,9 @@
 <div class="progress-wrapper">
     <div class="progress-bar">
         <ul>
-            <li class="step"><span class="numeric-indicator">1</span>Montant</li>
+            <li class="step done-text"><span class=" done numeric-indicator">1</span>Montant</li>
             <li class="logic-component"> > </li>
-            <li class="step"><span class="numeric-indicator">2</span>Paiement</li>
+            <li class="step current-text"><span class="current numeric-indicator">2</span>Paiement</li>
             <li class="logic-component"> > </li>
             <li class="step"><span class="numeric-indicator">3</span>Terminé !</li>
         </ul>
@@ -14,10 +14,10 @@
             <li class="step done-text"><span class="done numeric-indicator">1</span> Informations</li>
             <li class="logic-component"> > </li>
 
-            <li class="step current-text"><span class="numeric-indicator current">2</span>Montant</li>
+            <li class="step done-text"><span class="done numeric-indicator">2</span>Montant</li>
             <li class="logic-component"> > </li>
 
-            <li class="step"><span class="numeric-indicator">3</span>Paiement</li>
+            <li class="step current-text"><span class="current numeric-indicator">3</span>Paiement</li>
             <li class="logic-component"> > </li>
 
             <li class="step"><span class="numeric-indicator">4</span>Terminé !</li>
