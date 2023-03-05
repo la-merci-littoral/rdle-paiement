6a1a48348b96185041d5f36a2966062456221efe
Elias Kirkwood <yannis3736@gmail.com>
2023-03-05 22:17:36 +0100

    Commit all work cause searching a bug

--- modules/progress.php
+++ modules/progress.php
@@ -48,13 +48,13 @@
             <li class="step <?php echo $infoStatus[0] ?>"><span class="<?php echo $infoStatus[1] ?> numeric-indicator">1</span>Informations</li>
             <li class="logic-component"><span class="material-symbols-outlined progress-child">navigate_next</span></li>
             
-            <li class="step" <?php echo $amountStatus[0] ?>><span class="<?php echo $amountStatus[1] ?> numeric-indicator">2</span>Montant</li>
+            <li class="step <?php echo $amountStatus[0] ?>"><span class="<?php echo $amountStatus[1] ?> numeric-indicator">2</span>Montant</li>
             <li class="logic-component"><span class="material-symbols-outlined progress-child">navigate_next</span></li>
             
-            <li class="step" <?php echo $paymentStatus[0] ?>><span class="<?php echo $paymentStatus[1] ?> numeric-indicator">3</span>Paiement</li>
+            <li class="step <?php echo $paymentStatus[0] ?>"><span class="<?php echo $paymentStatus[1] ?> numeric-indicator">3</span>Paiement</li>
             <li class="logic-component"><span class="material-symbols-outlined progress-child">navigate_next</span></li>
             
-            <li class="step" <?php echo $successStatus[0] ?>><span class="<?php echo $successStatus[1] ?> numeric-indicator">4</span>Terminé !</li>
+            <li class="step<?php echo $successStatus[0] ?>"><span class="<?php echo $successStatus[1] ?> numeric-indicator">4</span>Terminé !</li>
 
         <?php } else { ?>
 
