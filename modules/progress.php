5919883fc984272fc96b767efe39b5aa6905c2ac
Elias Kirkwood <yannis3736@gmail.com>
2023-03-05 14:15:39 +0100

    Feat : CSS for page done and current page added

--- modules/progress.php
+++ modules/progress.php
@@ -2,17 +2,25 @@
 <div class="progress-wrapper">
     <div class="progress-bar">
         <ul>
-            <li>Montant</li>
-            <li>Paiement</li>
-            <li>Terminé !</li>
+            <li class="step"><span class="numeric-indicator">1</span>Montant</li>
+            <li class="logic-component"> > </li>
+            <li class="step"><span class="numeric-indicator">2</span>Paiement</li>
+            <li class="logic-component"> > </li>
+            <li class="step"><span class="numeric-indicator">3</span>Terminé !</li>
         </ul>
     </div>
     <div class="progress-bar">
         <ul>
-            <li>Informations</li>
-            <li>Montant</li>
-            <li>Paiement</li>
-            <li>Terminé !</li>
+            <li class="step done-text"><span class="done numeric-indicator">1</span> Informations</li>
+            <li class="logic-component"> > </li>
+
+            <li class="step current-text"><span class="numeric-indicator current">2</span>Montant</li>
+            <li class="logic-component"> > </li>
+
+            <li class="step"><span class="numeric-indicator">3</span>Paiement</li>
+            <li class="logic-component"> > </li>
+
+            <li class="step"><span class="numeric-indicator">4</span>Terminé !</li>
         </ul>
     </div>
 
