5ad10d562349fa7dfe772e19347ce4ecae2f7988
Elias Kirkwood <yannis3736@gmail.com>
2023-03-05 13:17:37 +0100

    Lay foundations for progress-bar

---
+++ modules/progress.php
@@ -0,0 +1,20 @@
+<link rel="stylesheet" href="../modules/progress-style.css">
+<div class="progress-wrapper">
+    <div class="progress-bar">
+        <ul>
+            <li>Choix</li>
+            <li>Montant</li>
+            <li>Paiement</li>
+            <li>Terminé !</li>
+        </ul>
+    </div>
+    <div class="progress-bar">
+        <ul>
+            <li>Choix</li>
+            <li>Informations</li>
+            <li>Montant</li>
+            <li>Paiement</li>
+            <li>Terminé !</li>
+        </ul>
+    </div>
+</div>
