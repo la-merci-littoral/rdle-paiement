2ab534588eb4d10ccd685a918189fd8685a37cf8
Elias Kirkwood <yannis3736@gmail.com>
2023-03-05 18:57:00 +0100

    Prepare progress bar for integration

--- modules/progress.php
+++ modules/progress.php
@@ -1,8 +1,29 @@
 <link rel="stylesheet" href="../modules/progress-style.css">
 <script src="../modules/progress.js" defer></script>
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-<div class="progress-wrapper">
-    <div class="progress-bar">
+
+
+<!-- Exemple of progress bar for people not choosing anonymous -->
+<div class="progress-bar">
+    <ul>
+        <li class="step done-text"><span class="done numeric-indicator">1</span> Informations</li>
+        <li class="logic-component"><span class="material-symbols-outlined">navigate_next</span></li>
+
+        <li class="step done-text"><span class="done numeric-indicator">2</span>Montant</li>
+        <li class="logic-component"><span class="material-symbols-outlined">navigate_next</span></li>
+
+        <li class="step current-text"><span class="current numeric-indicator">3</span>Paiement</li>
+        <li class="logic-component"><span class="material-symbols-outlined">navigate_next</span></li>
+
+        <li class="step"><span class="numeric-indicator">4</span>Terminé !</li>
+    </ul>
+</div>
+
+
+
+<!-- <div class="progress-wrapper"> -->
+    <!-- Exemple of progress bar for anonymous people -->
+    <!-- <div class="progress-bar">
         <ul>
             <li class="step done-text"><span class=" done numeric-indicator">1</span>Montant</li>
             <li class="logic-component"><span class="material-symbols-outlined">navigate_next</span></li>
@@ -12,28 +33,6 @@
 
             <li class="step"><span class="numeric-indicator">3</span>Terminé !</li>
         </ul>
-    </div>
-    <div class="progress-bar">
-        <ul>
-            <li class="step done-text"><span class="done numeric-indicator">1</span> Informations</li>
-            <li class="logic-component"><span class="material-symbols-outlined">navigate_next</span></li>
-
-            <li class="step done-text"><span class="done numeric-indicator">2</span>Montant</li>
-            <li class="logic-component"><span class="material-symbols-outlined">navigate_next</span></li>
-
-            <li class="step current-text"><span class="current numeric-indicator">3</span>Paiement</li>
-            <li class="logic-component"><span class="material-symbols-outlined">navigate_next</span></li>
-
-            <li class="step"><span class="numeric-indicator">4</span>Terminé !</li>
-        </ul>
-    </div>
-
-    <!-- <div class="progress">
-        <ul>
-            <li class="step">1</li>
-            <li class="step">2</li>
-            <li class="step">3</li>
-            <li class="step">4</li>
-        </ul>
     </div> -->
-</div>
+
+<!-- </div> -->
