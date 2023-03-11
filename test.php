18d9d9bcac4290c2176f674ea7cb9e1b4570b697
Elias Kirkwood <yannis3736@gmail.com>
2023-03-11 13:52:45 +0100

    Improve code of Elias

--- test.php
+++ test.php
@@ -5,6 +5,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Test</title>
+
+    <style>
+        html, body {
+            width: 100vw;
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background-color: #222;
+        }
+
+        #video {
+            width: 80vw;
+            height: 80vh;
+        }
+    </style>
+
 </head>
 <body>
     <iframe id="video" src="//www.youtube.com/embed/QDia3e12czc?rel=0&autoplay=1" frameborder="0" allowfullscreen></iframe>
