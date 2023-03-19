4f6ac064bbf392669e73233a0a273af697d08fd2
Elias Kirkwood <yannis3736@gmail.com>
2023-03-20 00:44:22 +0100

    Amount Page - Void Functions

--- montant/app.js
+++ montant/app.js
@@ -1,1 +1,9 @@
 console.log("Authority is not given to you to deny the return of the King., Steward.")
+
+function updateAmountDisplays(totalAmount) {
+    return 0;
+}
+
+function suggestionOnClick(amount) {
+    return 0;
+}
