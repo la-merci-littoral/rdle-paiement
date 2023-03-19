73edd2f07babc644f2a04d48d7c8208659482cd6
Elias Kirkwood <yannis3736@gmail.com>
2023-03-19 20:17:45 +0100

    Finish help button

--- modules/help.js
+++ modules/help.js
@@ -3,21 +3,32 @@ const helpText = document.getElementsByClassName('help-text')[0];
 helpText.style.display = "none";
 helpText.style.opacity = '0';
 
+// Creates a sleep function that works through the use of a Promise : Non-intrusive function
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+// On Hover event shows a text bubble
 helpWidget[0].addEventListener("mouseover", (element) => {
-    // console.log("Opacity: ", helpText.style.opacity, " | Display: ", helpText.style.display)
     if ((helpText.style.opacity == 0) && (helpText.style.display == "none")) {
-        helpText.style.opacity = '1';
+        helpText.style.display = "flex"
+        // Timeout is necessary for animation, don't ask why
         setTimeout(() => {
-            helpText.style.display = "flex"
-        }, 200)
+            helpText.style.opacity = '1';
+        }, 1)
     }
 })
 
+// Hides text bubble when hover stops
 helpWidget[0].addEventListener("mouseleave", (element) => {
     if ((helpText.style.opacity == 1) && (helpText.style.display == "flex")) {
-        helpText.style.opacity = '0';
-        setTimeout(() => {
-            helpText.style.display = 'none';
-        }, 300)
+        // Adds waiting time so that text still shows up if someone went over button quickly
+        sleep(300).then(() => {
+            helpText.style.opacity = '0';
+            // Timeout is necessary to let the opacity animation take place
+            setTimeout(() => {
+                helpText.style.display = 'none';
+            }, 300)
+        })
     }
 })
