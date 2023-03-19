315d3c0e26e2d1e9a8a7248d76926f3e0a9a2f78
Elias Kirkwood <yannis3736@gmail.com>
2023-03-20 00:34:04 +0100

    Add js file (already done under wrong commit)

--- montant/index.php
+++ montant/index.php
@@ -53,7 +53,8 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;500;600;700;800;900&display=swap" rel="stylesheet">
 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    
+    <script src="./app.js"></script>
+
     <title>Choix du Montant - Ronde de l'Espoir</title>
 </head>
 <body>
