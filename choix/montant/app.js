d77df3805841d7078a0202a13e7f75d5a67612a2
Elias Kirkwood <yannis3736@gmail.com>
2023-03-29 18:16:15 +0200

    Feat : Suggestion button under amount input work

--- choix/montant/app.js
+++ choix/montant/app.js
@@ -1,5 +1,11 @@
 console.log("Authority is not given to you to deny the return of the King., Steward.")
 
+
+function inputSuggestedAmount(suggestion) {
+    const input = document.querySelectorAll('#free-choice')[0];
+    input.value = suggestion;
+}
+
 function updateAmountDisplays(totalAmount) {
     return 0;
 }
