324dd06e9962658d28dfa50ab3166a75776ee660
Elias Kirkwood <yannis3736@gmail.com>
2023-04-01 16:08:32 +0200

    API Key in secure folder

--- config/db_connect.php
+++ config/db_connect.php
@@ -4,6 +4,7 @@
     $username = "ctzs1179";
     $password = "o2switch2022";
     $dbname = "ctzs1179_donations";
+    $inseeAPIKey = "b0872199-23cd-3fcb-8cbe-96abb5a6c802";
 
     $conn = new mysqli($servername, $username, $password, $dbname);
 
